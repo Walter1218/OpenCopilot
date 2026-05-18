d0554a7c3f63df5dd34345e4aa4fa1b14505dbf6
walter1218 <walter.1218@hotmail.com>
2026-05-19 01:36:52 +0800

    chore: remove test files

--- test_minimax.py
+++
@@ -1,7 +0,0 @@
-import os
-from llm_provider import ProviderFactory
-os.environ["MINIMAX_API_KEY"] = "sk-cp-5Ta1Ur5ytb4uy4HPVW9Pu6Gcox0-maiU4TGZ-GQs22JeGHPY-7jhoh2n0boUE6IUp9ilRJrMPQjaVNOP9Z61Lw-8qY8k7p0huF-vxcI6MuqNdaD3Jjxgap0"
-provider = ProviderFactory.create_provider()
-for chunk in provider.stream_chat("Hello", system_prompt="You are a helper."):
-    print(chunk, end="", flush=True)
-print("\nDone")
